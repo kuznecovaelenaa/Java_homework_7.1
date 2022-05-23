5ca4470aeb47638d16d92ae5a29e02e904ed8338
Elena Kuznetsova <kuznecovaelenaa@yandex.ru>
2022-05-23 12:31:00 +0300

    Changes pom & test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,8 +10,20 @@ public class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 13};
+        long expected = 13;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findSecondMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1, 2};
+        long expected = 2;
 
         long actual = service.findMax(incomesInBillions);
 
